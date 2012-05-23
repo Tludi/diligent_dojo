80febeb4284743b4473098afae0d7f93ce1a8b6c
Tim Ludi <tim@tortuga-mint.(none)>
2012-05-23 12:08:33 -0700

    baseline app, postgres, rspec, cucumber, no models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dojo::Application.config.secret_token = '2cf218e8a824c7ee25efe7c3267e8712cfa4b4575ec2dd686e2e5d80a9eb0f3522b1035f24e790f0f69395b059b7e3a7da34fdb3be85d80ba21b2f43569b8933'
